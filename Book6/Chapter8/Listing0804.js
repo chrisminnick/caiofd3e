434d7e3f49f2bfe6457d541afffc356e6c95c812
Chris Minnick <chris@minnick.com>
2026-03-19 17:19:42 -0700

    book 6 chapter 10

---
+++ Book6/Chapter8/Listing0804.js
@@ -0,0 +1,8 @@
+function greetInSpanish(name) {
+  return `Hola, ${name}`;
+}
+function getUserName(callback) {
+  let firstName = prompt('Enter your first name');
+  return callback(firstName);
+}
+getUserName(greetInSpanish);
---
+++ Book6/Chapter8/getLocalWeather.js
@@ -0,0 +1,4 @@
+function getLocalWeather(postalCode) {
+  const weather = `I don't know what the weather is like in ${postalCode}. Maybe try looking out the window.`;
+  return weather;
+}
