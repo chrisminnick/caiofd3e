640b31d2659072f869ef9dbf90ef2ab7c1379c8b
Chris Minnick <chris@minnick.com>
2026-04-06 18:13:05 -0700

    updating book 7

---
+++ Book7/Chapter1/listing070104.js
@@ -0,0 +1,20 @@
+sendLoginData(
+  args,
+  function (result) {
+    logIn(
+      result,
+      function (result) {
+        loadPage(
+          result,
+          function () {
+            console.log(`result: ${result}`);
+          },
+          failureCallback,
+        );
+      },
+      failureCallback,
+    );
+  },
+  failureCallback,
+);
+loginBtn.addEventListener('click', sendLoginData);
