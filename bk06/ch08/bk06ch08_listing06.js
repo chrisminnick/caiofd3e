6f27b75d13a2443f728762dfa082d07239450db5
Chris Minnick <chris@minnick.com>
2026-07-09 10:01:38 -0700

    bk06ch08: reconcile code with 3rd edition proof

---
+++ bk06/ch08/bk06ch08_listing06.js
@@ -0,0 +1,5 @@
+const convertMilesToKM = function (distanceInMiles) {
+  const distanceInKM = distanceInMiles * 1.609;
+  return distanceInKM;
+};
+console.log(convertMilesToKM(5));
---
+++ bk06/ch08/data/movies.js
@@ -0,0 +1,12 @@
+const movies = [
+  { title: 'The Great Gatsby' },
+  { title: 'Ulysses' },
+  { title: 'The Matrix' },
+  { title: 'Inception' },
+  { title: 'The Godfather' },
+  { title: 'Casablanca' },
+  { title: 'Jaws' },
+  { title: 'Alien' },
+  { title: 'Coco' },
+  { title: 'Spirited Away' },
+];
